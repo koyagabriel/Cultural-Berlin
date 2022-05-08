03a382f95bec97f2f1d046e58d62944603a5b4ff
koya <koyexes@gmail.com>
2022-05-08 12:59:46 +0100

    creates CulturalActivities controller - implement index action for fetching all cultural_activities - implement search action for filtering cultural_activities - registers index and search routes

--- cultural-activities-api/config/routes.rb
+++ cultural-activities-api/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/activities", controller: :cultural_activities, action: :index
+  get "/activities/search", controller: :cultural_activities, action: :search
 end
